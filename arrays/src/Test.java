eef57dbc177d40488b3d4cf907ea33bb701230e7
anuragbhu <anurag.bhumca18@gmail.com>
2021-11-28 21:35:00 +0530

    Added pattern problems.

--- arrays/src/Test.java
+++ arrays/src/Test.java
@@ -1,6 +1,13 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello Arrays");
+        int i = 1, n = 5;
+        while(i <= n) {
+            System.out.println("Hello");
+            i++;
+        }
     }
 }
---
+++ patterns/src/StarPattern.java
@@ -0,0 +1,6 @@
+public class StarPattern {
+
+    public static void main(String[] args) {
+
+    }
+}
