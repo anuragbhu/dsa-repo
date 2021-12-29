d312d8ac10f8d775a2d8053293ea5a07f6ea01df
anuragbhu <anurag.bhumca18@gmail.com>
2021-12-30 01:51:39 +0530

    Adding new Prime Seive, Moore's Voting Algorithm, Dutch National Flag Algorithm problems.

--- arrayalgorithmone/src/SieveOfEratosthenes.java
+++ arrayalgorithmone/src/SieveOfEratosthenes.java
@@ -6,13 +6,80 @@ public class SieveOfEratosthenes {
         ArrayList<Integer> res = new ArrayList<Integer>();
 
         for (int i = 2; i <= N; i++) {
-            int count = 0;
-            for(int j = 2; j <= N; j++) {
+            boolean isPrime = true;
+            for(int j = 2; j < i; j++) {
                 if(i % j == 0) {
-                    count++;
+                    isPrime = false;
+                    break;
                 }
             }
-            if(count == 1) res.add(i);
+
+            if(isPrime) res.add(i);
+        }
+
+        return res;
+    }
+
+    static ArrayList<Integer> sieveOfEratosthenes1(int N){
+
+        ArrayList<Integer> res = new ArrayList<Integer>();
+
+        for (int i = 2; i <= N; i++) {
+            boolean isPrime = true;
+            for(int j = 2; j <= i/2; j++) {
+                if(i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if(isPrime) res.add(i);
+        }
+
+        return res;
+    }
+
+    static ArrayList<Integer> sieveOfEratosthenes2(int N){
+
+        ArrayList<Integer> res = new ArrayList<Integer>();
+
+        for (int i = 2; i <= N; i++) {
+            boolean isPrime = true;
+            for(int j = 2; j <= Math.sqrt(i); j++) {
+                if(i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if(isPrime) res.add(i);
+        }
+
+        return res;
+    }
+
+    static ArrayList<Integer> sieveOfEratosthenes3(int N){
+
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        boolean[] prime = new boolean[N+1];
+
+        for(int i = 0; i <= N; i++) {
+            prime[i] = true;
+        }
+
+        prime[0] = false;
+        prime[1] = false;
+
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+
+            if(prime[i]) {
+                for(int j = 2; (i * j) <= N; j++)
+                    prime[i*j] = false;
+            }
+        }
+
+        for(int i = 0; i <= N; i++) {
+            if(prime[i]) res.add(i);
         }
 
         return res;
--- arrays/src/Test.java
+++ arrays/src/Test.java
@@ -4,5 +4,8 @@ import java.util.*;
 public class Test {
 
     public static void main(String[] args) {
+        int N = 5;
+        boolean prime[] = new boolean[N+1];
+        System.out.println(prime[3]);
     }
 }
