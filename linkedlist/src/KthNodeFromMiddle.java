73d0fc5c0ee5bc8434db88a61b4947e125fd0017
anuragbhu <anurag.bhumca18@gmail.com>
2022-01-26 00:24:25 +0530

    Adding new Linked List problems.

--- linkedlist/src/KthNodeFromMiddle.java
+++ linkedlist/src/KthNodeFromMiddle.java
@@ -24,4 +24,33 @@ public class KthNodeFromMiddle {
         }
         return -1;
     }
+
+    public int solve1(ListNode A, int B) {
+        ListNode curr = A;
+        ListNode fast = A;
+        ListNode slow = A;
+        int count = 0; // For length of the LL
+
+        // Finding length & mid of the LL
+        while(fast != null && fast.next != null) {
+            count++;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        count = (count * 2) + 1;
+        if(count < B) return -1;
+
+        //  Bth node from the middle towards the beginning of the Linked List A
+        int mid = (count / 2) + 1 - B;
+        count = 0;
+        curr = A;
+
+        while(curr != null) {
+            count++;
+            if(count == mid)
+                return (int) curr.val;
+            curr = curr.next;
+        }
+        return -1;
+    }
 }
