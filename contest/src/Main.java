72d3afb2eff3ba97f5f9af4193dd591164bdc3e1
anuragbhu <anurag.bhumca18@gmail.com>
2022-04-22 09:48:37 +0530

    added new dp problems.

--- contest/src/Main.java
+++ contest/src/Main.java
@@ -3,7 +3,6 @@ import java.math.*;
 import java.lang.*;
 
 public class Main {
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
---
+++ dynamicprogramming/src/Pattern.java
@@ -0,0 +1,96 @@
+public class Pattern {
+    public static void main(String[] args) {
+        /*
+0/1 Knapsack (Bounded)
+	> Subset sum
+	> Equal sum partition
+	> Count of subsets sum with a given sum
+	> Minimum subset sum difference
+	> Count the number of subset with a given difference
+	> Target sum
+
+0/1 Knapsack (Unbounded)
+	> Integer Break
+	> Coin Change
+	> Coin Change 2
+	> Combination Sum IV
+	> Perfect Squares
+	> Rod Cutting
+	> Maximum Ribbon Cut
+	> Minimum Cost For Tickets
+
+Longest Common Subsequence (LCS)
+	> Longest common substring
+	> Print LCS
+	> Shortest common supersequence (SCS)
+	> Print SCS
+	> Minimum number of insertion and deletion to convert A to B
+	> Longest repeating subsequence
+	> Length of longest subsequence of A which is substring of B
+	> Subsequence pattern matching
+	> Count how many times A appears as subsequence in B
+	> Longest palindromic subsequence
+	> Longest palindromic substring
+	> Count of palindromic substrings
+	> Minimum number of deletion in a string to make it palindrome
+	> Minimum number of insertion in a string to make it palindrome
+	> Edit Distance
+	> Distinct Subsequences
+
+
+Matrix Chain Multiplication (MCM)
+	> Printing MCM
+	> Burst Balloons
+	> Evaluate expression to true / boolean parenthesization
+	> Minimum or maximum value of a expression
+	> Palindrome partitioning II
+	> Palindrome partitioning
+	> Egg Dropping Problem
+
+Fibonacci
+	> Fibonacci number
+	> Climbing stairs
+	> Jump Game II (Minimum jumps to reach the end)
+	> Min Cost Climbing Stairs (Minimum jumps with fee)
+	> Friends pairing problem
+	> Maximum sum such that no two elements are adjacent
+	> Maximum subsequence sum such that no three are consecutive
+
+Longest Increasing Subsequence (LIS)
+	> Print longest increasing subsequence
+	> Number of longest increasing subsequences
+	> Find the longest increasing subsequence in circular manner
+	> Longest bitonic subsequence
+	> Longest arithmetic subsequence
+	> Maximum sum increasing subsequence
+	> Delete and Earn
+	> Longest String Chain
+
+DP on Trees
+	> Diameter of Binary Tree
+	> Binary Tree Maximum Path Sum
+	> Unique Binary Search Trees II
+	> House Robber III
+
+DP on Grids/Matrix/2D Array
+	> Unique Paths
+	> Unique Paths II
+	> Minimum Path Sum
+	> Cherry Pickup
+	> Range Sum Query 2D - Immutable
+	> Triangle
+	> Maximal Square
+
+BitMasking
+	> Partition to K Equal Sum Subsets
+
+State machine
+	> Best Time to Buy and Sell Stock
+	> Best Time to Buy and Sell Stock I
+	> Best Time to Buy and Sell Stock III
+	> Best Time to Buy and Sell Stock IV
+	> Best Time to Buy and Sell Stock with Cooldown
+	> Best Time to Buy and Sell Stock with Transaction Fee
+         */
+    }
+}
