f9e6951e40bb8db8491973936fa1c1767e06b3e6
anuragbhu <anurag.bhumca18@gmail.com>
2022-04-25 00:14:14 +0530

    added two pointers problems.

--- twopointers/src/MaximumOnesAfterModification.java
+++ twopointers/src/MaximumOnesAfterModification.java
@@ -19,4 +19,25 @@ public class MaximumOnesAfterModification {
         }
         return ans;
     }
+
+    // TC = O(n), AS = O(1)
+    public int solve1(ArrayList<Integer> A, int B) {
+        int start = 0, end = 0;
+        int result = Integer.MIN_VALUE;
+
+        while(end < A.size()) {
+            if(A.get(end) == 0) {
+                B--;
+            }
+            while(B < 0) {
+                if(A.get(start) == 0) {
+                    B++;
+                }
+                start++;
+            }
+            result = Math.max(result, end - start + 1);
+            end++;
+        }
+        return result;
+    }
 }
